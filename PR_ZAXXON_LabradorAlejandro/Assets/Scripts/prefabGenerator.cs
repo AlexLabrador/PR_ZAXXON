fc6762b3749ffc4734e5673966fbafc166340c10
alex labrador <alejandrolabradordorado@gmail.com>
2021-10-05 13:57:22 +0200

    Random obstaculos
    los obstáculos se generan aleatoriamente y en un tiempo aleatorio también

--- PR_ZAXXON_LabradorAlejandro/Assets/Scripts/prefabGenerator.cs
+++ PR_ZAXXON_LabradorAlejandro/Assets/Scripts/prefabGenerator.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class prefabGenerator : MonoBehaviour
 {
+    float intervalo;
     [SerializeField] Transform posInst;
     [SerializeField] GameObject MyPrefab;
+    public Vector3 posicion;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(MyPrefab, posInst);
+
+        StartCoroutine("CrearObstaculos");
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator CrearObstaculos()
+    {
+        while (true)
+        {
+            posicion = new Vector3(Random.Range(-100f, 100f),Random.Range(0f,60f), Random.Range(80f, 100f));
+           intervalo = Random.Range(1f, 2f);
+            Instantiate(MyPrefab, posicion, Quaternion.identity);
+            yield return new WaitForSeconds(intervalo);
+        }
     }
 }
