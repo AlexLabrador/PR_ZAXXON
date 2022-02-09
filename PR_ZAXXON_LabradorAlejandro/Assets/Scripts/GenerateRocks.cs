b9829d32f1ac3f16137c5f00f31491f66d98101c
Natalia Gomez Anton <alejandrolabradordorado@gmail.com>
2022-02-09 09:57:37 +0100

    he actualizado al URP y he creado una segunda opción visual
    estoy eligiendo por donde será mi juego

---
+++ PR_ZAXXON_LabradorAlejandro/Assets/Scripts/GenerateRocks.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float intervalo;
+    [SerializeField] Transform posInst;
+    [SerializeField] GameObject[] MyPrefab;
+    [SerializeField] GameObject initObject;
+
+    InitGameScript initGameScript;
+
+    public Vector3 posicion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initObject = GameObject.Find("initGameScripts");
+        initGameScript = initObject.GetComponent<InitGameScript>();
+        StartCoroutine("CrearObstaculos");
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator CrearObstaculos()
+    {
+        while (true)
+        {
+            int randomNum = Random.Range(0, MyPrefab.Length);
+            ; posicion = new Vector3(Random.Range(-100f, 100f), 0f, transform.position.z + 100f);
+            //intervalo = Random.Range(0.5f, 1f);
+            intervalo = initGameScript.aparicionObs;
+            Instantiate(MyPrefab[randomNum], posicion, Quaternion.identity);
+
+            yield return new WaitForSeconds(intervalo);
+        }
+
+    }
+
+}
